27bc8683e32fc3a6ce1440f4cf617c65880ceca5
Lucas Bickel <hairmare@purplehaze.ch>
2012-04-26 21:25:05 +0200

    added Manifest autoupdating

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -6,6 +6,8 @@ mkdir -p $DISTDIR
 
 # rebuild everything :) i should start failing on this sonnish
 find . -name *.ebuild -exec ebuild {} digest \;
+find . -name 'Manifest' -exec git add {} \;
+git commit -m 'auto update digests' 
 
 # pack what clients need
 tar --exclude=README --exclude=repositories.xml --exclude=_scripts/ -cjf overlay.tar.bz2 ./
